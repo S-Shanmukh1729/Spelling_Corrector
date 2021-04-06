0eefd50ad90762d25cc0640e3b833e91710272ce
S-Shanmukh1729 <80630137+S-Shanmukh1729@users.noreply.github.com>
2021-04-06 10:57:15 +0530

    Update sentence_checker.py

--- sentence_checker.py
+++ sentence_checker.py
@@ -1,7 +1,7 @@
 ## Scentence corrector
 import requests
 word=input("Enter sentence: ")
-x=len(word.split())
+#x=len(word.split())
 urls="https://www.google.com/search?q="+word+"&oq="+word+"&aqs=chrome..69i57j46i10j0i10l7.3288j0j9&sourceid=chrome&ie=UTF-8"
 x=requests.get(urls)
 zz=str(x.content)
